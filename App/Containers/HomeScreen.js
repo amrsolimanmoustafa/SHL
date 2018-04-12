d758afb4d98a0c81d8c33806398fe7e688235b98
SABRYX <mohammedsabryabed@gmail.com>
2018-04-12 19:49:28 +0200

    MAP

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import { ScrollView, Text, KeyboardAvoidingView } from 'react-native'
+import { View, Text, KeyboardAvoidingView } from 'react-native'
 import { connect } from 'react-redux'
+import MapView from 'react-native-maps';
 // Add Actions - replace 'Your' with whatever your reducer is called :)
 // import YourActions from '../Redux/YourRedux'
 
@@ -8,14 +9,23 @@ import { connect } from 'react-redux'
 import styles from './Styles/HomeScreenStyle'
 
 class HomeScreen extends Component {
-  render () {
+ 
+
+
+  render() {
     return (
-      <ScrollView style={styles.container}>
-        <KeyboardAvoidingView behavior='position'>
-          <Text>HomeScreen</Text>
-        </KeyboardAvoidingView>
-      </ScrollView>
-    )
+      <View style={{flex: 1}}>
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}>
+        </MapView>
+      </View>
+    );
   }
 }
 
--- App/Containers/Styles/HomeScreenStyle.js
+++ App/Containers/Styles/HomeScreenStyle.js
@@ -2,5 +2,21 @@ import { StyleSheet } from 'react-native'
 import { ApplicationStyles } from '../../Themes/'
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 })
