83b38c368c8c6caa2c26f2736ea70d99f7e0b84f
SABRYX <mohammedsabryabed@gmail.com>
2018-04-18 02:12:07 +0200

    MapScreen__addSideBar+TwoButtonsAtTheEnd

--- App/Components/Map.js
+++ App/Components/Map.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity,Image } from 'react-native'
 import styles from './Styles/MapStyle'
 import MapView from 'react-native-maps';
+import { Icon, Button } from 'native-base';
+import {Images} from '../Themes';
+import SideMapButtons from "../Components/SideMapButtons"
+import MainButtons from "../Components/MainButtons"
 
 export default class Map extends Component {
   // // Prop type warnings
@@ -28,6 +32,12 @@ export default class Map extends Component {
             longitudeDelta: 0.0421,
           }}>
         </MapView>
+        <TouchableOpacity style={styles.searchTouchable}>
+          <Text style={styles.searchTouchableText}> البحث </Text>
+          <Icon name="search" type="EvilIcons" style={styles.searchTouchableIcon}/>
+        </TouchableOpacity>
+          <SideMapButtons/>
+          <MainButtons/>
       </View>
     )
   }
---
+++ App/Components/Styles/SideMapButtonsStyle.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  SideComponents:{
+    marginLeft:"90%",
+    backgroundColor:"transparent",
+    height:"40%",
+    width:"10%",
+    marginTop:"18%",
+    borderRadius:100,
+    justifyContent:"space-between",
+
+  },
+  touchable:{
+    height:"25%",
+    width:"100%",
+    borderRadius:100,
+  },
+  image:{
+    height:"100%",
+    width:"100%"
+  }
+})
