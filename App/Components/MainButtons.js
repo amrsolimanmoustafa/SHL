4fb6cd74bb2aa0c94c0671fdb4fb50e2787c0a97
SABRYX <mohammedsabryabed@gmail.com>
2018-04-22 16:19:51 +0200

    MainScreenDone_withoutScrolableMenu_withoutStateChanges

--- App/Components/MainButtons.js
+++ App/Components/MainButtons.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types';
-import { View, Text,Button,TouchableOpacity } from 'react-native'
+import { View, Text,Button,TouchableOpacity,Image } from 'react-native'
 import styles from './Styles/MainButtonsStyle'
-import LinearGradientButton from "../Components/LinearGradientButton"
+import LinearGradientForMap from "../Components/LinearGradientForMap"
+import { Images } from '../Themes';
 
 export default class MainButtons extends Component {
   // // Prop type warnings
@@ -19,15 +20,35 @@ export default class MainButtons extends Component {
   render () {
     return (
       <View style={styles.container}>
+
+          <View style={styles.chooseServiceView2}>
+          <TouchableOpacity  style={styles.chooseServiceButton2}>
+          <Image source={Images.pickup} style={styles.chooseServiceImage2}/></TouchableOpacity>
+          <TouchableOpacity  style={styles.chooseServiceButton2}>
+          <Image source={Images.pluming} style={styles.chooseServiceImage2}/></TouchableOpacity>
+          <TouchableOpacity  style={styles.chooseServiceButton2}>
+          <Image source={Images.waterconnect} style={styles.chooseServiceImage2}/></TouchableOpacity>
+        </View>
+        <View style={styles.chooseServiceView}>
+          <TouchableOpacity  style={styles.chooseServiceButton}>
+          <Image source={Images.pickup} style={styles.chooseServiceImage}/></TouchableOpacity>
+          <TouchableOpacity  style={styles.chooseServiceButton}>
+          <Image source={Images.pluming} style={styles.chooseServiceImage}/></TouchableOpacity>
+          <TouchableOpacity  style={styles.chooseServiceButton}>
+          <Image source={Images.waterconnect} style={styles.chooseServiceImage}/></TouchableOpacity>
+          <TouchableOpacity  style={styles.chooseServiceButton}>
+          <Image source={Images.stahat} style={styles.chooseServiceImage}/></TouchableOpacity>
+        </View>
         
+        <View style={styles.whenToOrderView}>
         <View style={styles.opacityView}>
          <TouchableOpacity style={styles.opacityWight}><Text style={styles.opacityWightText}>اطلب لاحقاً</Text></TouchableOpacity>
         </View>
 
         <View style={styles.opacityView2}>
-          <LinearGradientButton navigateScreen="VerifyPhoneScreen" text="اطلب الان" stylez={styles.opacity}/>
+          <LinearGradientForMap  text="اطلب الان" stylez={styles.opacity}  press={() => this.props.navigation.navigate("HomeScreen")} />
         </View>
-       
+       </View>
         
       </View>
     )
--- App/Components/Styles/MainButtonsStyle.js
+++ App/Components/Styles/MainButtonsStyle.js
@@ -3,45 +3,113 @@ import { StyleSheet } from 'react-native'
 export default StyleSheet.create({
   container: {
     flex: 1,
-    marginTop:"20%",
+    marginTop:"10%",
+    flexDirection:"column",
+    alignItems:"center",
+    height:"100%",
+  },
+  whenToOrderView:{
     flexDirection:"row",
     alignItems:"center",
+    height:"25%",
+    marginTop:"3%"
   },
   opacityView:{
-    marginTop:"10%",
+    marginTop:"0%",
     borderRadius: 100,
-    width:"50%",
+    width:"45%",
     height:"90%",
-    marginLeft:"10%"
+    marginLeft:"10%",
+    elevation :10
 
   },
   opacityView2:{
-    marginTop:"10%",
+    marginTop:"0%",
     borderRadius: 100,
-    width:"50%",
+    width:"45%",
     height:"90%",
     marginRight:"10%",
+    elevation:10,
+    marginLeft:"5%"
 
 
   },
   opacity: {
-    marginTop:"12%",
+    marginTop:"0%",
     borderRadius: 80,
-    width:"70%",
+    width:"100%",
     height:"100%",
+    alignItems:"center",
   },
   opacityWight: {
-    marginTop:"12%",
+    marginTop:"0%",
     borderRadius: 80,
-    width:"65%",
-    height:"30%",
+    width:"100%",
+    height:"100%",
     backgroundColor:"white",
     alignItems:"center",
-    marginLeft:"30%"
+    marginLeft:"0%"
   },
   opacityWightText:{
     marginTop:"8%",
     fontSize:18
-  }
+  },
+  chooseServiceView:{
+    height:"30%",
+    width:"80%",
+    flexDirection:"row",
+    backgroundColor:"#rgba(255, 255, 255, 0.4)",
+    borderRadius:100,
+    justifyContent:"space-between",
+    elevation:10,
+    marginTop:"1%",
+    
+
+  },
+  chooseServiceButton:{
+    backgroundColor:"white",
+    borderRadius:100,
+    height:"100%",
+    width:"22%",
+    elevation:10,
+    alignItems:"center",
+    marginLeft:"2%",
+    justifyContent:"center",
+    borderWidth:2,
+    borderColor:"#rgb(57,180,76)"
+    },
+    chooseServiceImage:{
+      height:"70%",
+      width:"70%",
+
+    },
+    chooseServiceView2:{
+      height:"25%",
+      width:"50%",
+      flexDirection:"row",
+      backgroundColor:"#rgba(255, 255, 255, 0.4)",
+      borderRadius:100,
+      justifyContent:"space-between",
+      elevation:10
+      
+  
+    },
+    chooseServiceButton2:{
+      backgroundColor:"white",
+      borderRadius:100,
+      height:"100%",
+      width:"30%",
+      elevation:10,
+      alignItems:"center",
+      marginLeft:"2%",
+      justifyContent:"center",
+      borderWidth:2,
+      borderColor:"#rgb(57,180,76)"
+      },
+      chooseServiceImage2:{
+        height:"65%",
+        width:"65%",
+  
+      },
 
 })
