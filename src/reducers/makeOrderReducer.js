ac1a910285e3fa1874ab9feb08c5c14ba12f0816
HeshamEG <heshamsalama1212901022@gmail.com>
2018-04-25 18:11:25 +0200

    servecis setup in mainbuttons componnent

--- src/reducers/makeOrderReducer.js
+++ src/reducers/makeOrderReducer.js
@@ -1,12 +1,14 @@
 import {GETSERVESIES} from '../actions/types'
 const initialState={
-    services:[]
+    services:[],
+    service:[]
 }
 export default function(state=initialState,action){
+    console.log(action.payload)
     switch(action.type){
         case GETSERVESIES:
         return {...state,
-            services:action.payload}
+            services:action.payload,service:action.payload.data}
         default:
         return state;
     }
